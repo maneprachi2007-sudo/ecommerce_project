c32d77a9079c3c61068bbd0b4abe646bf0b5f536
prachiee29 <maneprachi2007@gmai.com>
2026-06-24 00:17:02 +0530

    First Commit

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import pymysql
 
 connection = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="maneprachi2007",
+    host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    port=4000,
+    user="jv3Yu2NL1ExHgCF.root",
+    password="XmrYNrxXiEuoIc03",
     database="ecommerce_project"
 )
