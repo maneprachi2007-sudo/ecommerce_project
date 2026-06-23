d54d80555077343424865bbec6c8130cbaba7bbd
prachiee29 <maneprachi2007@gmai.com>
2026-06-24 00:43:50 +0530

    Updated TiDB connection

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ connection = pymysql.connect(
     host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
     user="jv3Yu2NL1ExHgCF.root",
     password="XmrYNrxXiEuoIc03",
-    database="ecommerce_project"
+    database="ecommerce_project",
     port=4000
 )
 
